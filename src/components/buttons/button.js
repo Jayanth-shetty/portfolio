ce4f1c85d9ca94675fedf7003de2b7c041d95a2f
jayanth <jayanthshetty660@gmail.com>
2025-12-08 00:33:21 +0530

    updated  buttons

---
+++ src/components/buttons/button.js
@@ -0,0 +1,17 @@
+"use client";
+export default function Button({children,action,...props}){
+    function handelSelect(){
+    if(action==='Resume'){
+      const link=document.createElement('a');
+      link.href='/jayanth_Resume.pdf';
+      link.download='jayanth_Resume.pdf'
+      link.click();
+    }
+    if(action==='call'){
+        window.location.href="tel:+918867094596"
+    }
+  }
+    return(
+        <button onClick={handelSelect}{...props}>{children}</button>
+    )
+}
--- src/components/main-content/mainContent.js
+++ src/components/main-content/mainContent.js
@@ -1,20 +1,29 @@
+import Button from "../buttons/button";
 import classes from "./maincontent.module.css";
 import ProfilePicture from "./profile/profilePicture";
 import SvgIcons from "./svgIcons";
 export default function MainContent() {
+  
   return (
     <main className={classes.container}>
       <div className={classes.part1}>
         <div className={classes.selfStart}>
           <h1>hey i am jayanth</h1>
-          <h1>scscs</h1>
+          <p>
+            I&apos;m a software developer skilled in front-end,focused on
+            creating dynamic web experiences. Let&apos;s connect and turn your
+            ideas into reality!
+          </p>
         </div>
         <div className={classes.selfEnd}>
-          <button>Get in touch</button>
+          <Button action="call" className='black-btn'>Get In Touch</Button>
+          <Button action="Resume" className='blue-btn'>Download Resume</Button>
         </div>
       </div>
       <div className={classes.part2}>
-        <div className={classes.selfStart}><ProfilePicture /></div>
+        <div className={classes.selfStart}>
+          <ProfilePicture />
+        </div>
         <div className={classes.selfEnd}>
           <SvgIcons />
         </div>
