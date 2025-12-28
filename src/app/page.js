1a390270bd63f63953635963cca6731204f3f47c
jayanth <jayanthshetty660@gmail.com>
2025-12-29 01:16:27 +0530

    reduced size

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
-import Header from "@/components/header";
 import MainContent from "@/components/main-content/mainContent";
-import Image from "next/image";
 
 export default function Home() {
   return (
